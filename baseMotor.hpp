04a1d75572628ef3abe6b089f8f070a04fcce5a2
Jackson Case <126780741+NO-skcaj@users.noreply.github.com>
2024-06-12 10:38:53 -0400

    Create baseMotor.hpp
    This is the base class to be inherited by the other motors

---
+++ baseMotor.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace CMI
+{
+class baseMotor
+{
+  public:
+    // Initializaiton
+    baseMotor();
+    // Configs
+    void SetPID(); // Ideally most config will be done here with per-brand configs, but will be eventually changed to out own config type
+    // Set commands aka power the motor
+    void SetSpeed(); // Will go through PID
+    void SetAngle();
+    // Get commands aka telemetry
+    double GetSpeed();
+    double GetAngle();
+    double GetDistance();
+  private:
+    // PID values
+    float P, I, D, S, V, A, G;
+    // Etc
+    int CANID = 0;
+
+};
+} // CMI
